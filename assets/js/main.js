5d9d908166f77660b8a62d8cb9441cb6ef4db2d8
Chase <masksantos@tip.edu.ph>
2025-05-23 07:26:48 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,9 +34,73 @@ var learningsSwiper = new Swiper(".learnings-swiper", {
 
 /* Active work */
 
-/*=============== TESTIMONIALS SWIPER ===============*/
+/*=============== RESUME ===============*/
+const accordionItems = document.querySelectorAll('.resume-item');
+
+accordionItems.forEach((item) => {
+    const header = item.querySelector('.resume-header'),
+    content = item.querySelector('.resume-content'),
+    icon = item.querySelector('.resume-icon i');
+
+    header.addEventListener('click', () => {
+        const isOpen = item.classList.toggle('accordion-open');
+
+        content.style.height = isOpen ? content.scrollHeight + 'px' : '0';
+        icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
+
+        accordionItems.forEach((otherItem) => {
+            if (
+                otherItem !== item && 
+                otherItem.classList.contains('accordion-open')
+            ) {
+                otherItem.querySelector('.resume-content').style.height = '0';
+                otherItem.querySelector('.resume-icon i').classList= 'ri-add-line'
+                otherItem.classList.remove('accordion-open');
+            }
+        });
+    });
+});
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactSubject = document.getElementById('contact-subject'),
+contactMessage = document.getElementById('contact-message'),
+message = document.getElementById('message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if(contactName.value === '' || contactEmail.value === '' ||
+    contactSubject.value === '' || contactMessage.value === '') {
+        message.classList.add('color-red');
+        message.textContent = 'Write all the input fields.';
+
+        setTimeout(() => {
+            message.textContent = '';
+        }, 3000);
+    } else {
+      emailjs.sendForm('service_h6j3t3n', 
+        'template_35kicgb', 
+        '#contact-form', 
+        'FpmyZDwuI5UzH-G6M')
+      .then(
+        () => {
+            message.classList.add('color-first')
+            message.textContent = 'Message sent successfully.';
+
+            setTimeout(() => {
+                message.textContent = '';
+            }, 5000);
+        },
+        (error) => {
+            alert('Please try again.', error)
+        });
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== STYLE SWITCHER ===============*/
 const styleSwitcher = document.getElementById('style-switcher'),
